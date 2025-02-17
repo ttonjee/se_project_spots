f0788f0b9849e2ecef123381a300302ba60f87de
texan.tonje63@gmail.com <flexx_tex@Texans-MacBook-Pro.local>
2025-02-16 21:07:06 -0600

    linked image and txt content in javascripts

--- scripts/index.js
+++ scripts/index.js
@@ -32,13 +32,46 @@ const initialCards = [
 console.log(initialCards);
 
 const profileEditButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 const profileEditModal = document.querySelector("#profile__edit-modal");
-
+const profileEditFormElement = document.querySelector(".modal__form");
 const profileEditModalCloseButton =
   profileEditModal.querySelector(".modal__close-btn");
+const profileEditModalNameInput = profileEditModal.querySelector(
+  "#profile__name-input"
+);
+const profileEditModalDescriptionInput = profileEditModal.querySelector(
+  "#profile__description-input"
+);
+
+const cardsList = document.querySelector(".cards__list");
+
+const cardTemplate = document.querySelector("#card__template");
+
+function getCardElement(data) {
+  console.log(data);
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardElementDescription =
+    cardElement.querySelector(".card__description");
+  cardElementDescription.textContent = data.name;
+
+  const cardElementImage = cardElement.querySelector(".card__image");
+  cardElementImage.src = data.link;
+
+  const cardElementAlt = cardElement.querySelector(".alternate");
+  cardElementImage.alt = data.alt;
+
+  return cardElement;
+}
 
 function openModal() {
+  profileEditModalNameInput.value = profileName.textContent;
+  profileEditModalDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
 }
 
@@ -46,10 +79,23 @@ function closeModal() {
   profileEditModal.classList.remove("modal__opened");
 }
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileEditModalNameInput.value;
+  profileDescription.textContent = profileEditModalDescriptionInput.value;
+  closeModal();
+}
+
 profileEditButton.addEventListener("click", function () {
   openModal();
 });
-
 profileEditModalCloseButton.addEventListener("click", function () {
   closeModal();
 });
+profileEditFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  console.log(getCardElement(initialCards[i]));
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.append(cardElement);
+}
